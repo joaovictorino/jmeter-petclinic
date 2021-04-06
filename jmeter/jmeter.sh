6dee3968dca929267dbcdd70dd159e80ee956f2f
joaovictorino <jhvictorino@gmail.com>
2021-04-06 18:26:16 -0300

    first test

---
+++ jmeter/jmeter.sh
@@ -0,0 +1,1 @@
+./jmeter -n -t /home/joao/Source/Test/jmeter-petclinic-tests/jmeter/queryPet.jmx -l /home/joao/Source/Test/jmeter-petclinic-tests/jmeter/restBank.jtl -e -o /home/joao/Source/Test/jmeter-petclinic-tests/jmeter/report
